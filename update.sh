0c0fd3e24bb7b8d839e5b95c5890533fb6b70195
openhoangnc <hoangnc.jp@gmail.com>
2023-11-26 23:30:30 +0700

    Fix folder and file creation in update.sh script

--- update.sh
+++ update.sh
@@ -18,26 +18,21 @@ for line in "${csvLines[@]}" ; do
     device=$(echo $line | cut -d ',' -f 3)
     model=$(echo $line | cut -d ',' -f 4)
     if [ "$brand" != "" ] && [ "$marketing_name" != "" ] && [ "$device" != ""] && [ "$model" != "" ] ; then
-        #if not exist create brand folder
         if [ ! -d "$brand" ]; then
             mkdir "$brand"
         fi
-        #if not exist create device file with marketing_name content
+
+        if [[ "$device" == *"/"* ]]; then
+            device=$(echo "$device" | sed 's/\//-/g')
+        fi
         if [ ! -f "$brand/$device" ]; then
-            # if device contains "/"
-            if [[ "$device" == *"/"* ]]; then
-                # replace "/" with "-"
-                device=$(echo "$device" | sed 's/\//-/g')
-            fi
             echo "$marketing_name" > "$brand/$device"
         fi
-        #if not exist create model file with marketing_name content
+
+        if [[ "$model" == *"/"* ]]; then
+            model=$(echo "$model" | sed 's/\//-/g')
+        fi
         if [ ! -f "$brand/$model" ]; then
-            # if model contains "/"
-            if [[ "$model" == *"/"* ]]; then
-                # replace "/" with "-"
-                model=$(echo "$model" | sed 's/\//-/g')
-            fi
             echo "$marketing_name" > "$brand/$model"
         fi
     fi
